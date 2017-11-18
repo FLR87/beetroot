0a795eafab02ada29853296c9fec53d8e897e614
FLR87 <galeonis87@gmail.com>
2017-11-18 17:19:26 +0200

    my commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //$int = 1;
 //$string = '23';
 
-$array = [1, 3, 5, 6, 7, 8, 9, 'users' => ['Vadim', 'Alex', 'Sanya']];
+//$array = [1, 3, 5, 6, 7, 8, 9, 'users' => ['Vadim', 'Alex', 'Sanya']];
 
 //$arrayNames = ['Petrov','Ivanov','Sidorov'];
 //for ($i = 0; $i < 15; $i++) {
@@ -13,14 +13,14 @@ $array = [1, 3, 5, 6, 7, 8, 9, 'users' => ['Vadim', 'Alex', 'Sanya']];
 
 //}
 
-foreach ($array as $key => $value) {
-    if (is_array($value)) {
-        foreach ($value as $key => $value) {
-            var_dump($value);
-        }
-    }
-
-}
+//foreach ($array as $key => $value) {
+//    if (is_array($value)) {
+//        foreach ($value as $key => $value) {
+//            var_dump($value);
+//        }
+//    }
+//
+//}
 //while ($i < 5) {
 //    $count = $i++;
 //    echo '<pre>';
@@ -35,6 +35,70 @@ foreach ($array as $key => $value) {
 //
 //}
 
+//var_dump ($_SERVER);
+//var_dump ($_GET['id]']);
+//var_dump ($_POST['id]']);
 
+//$b = 5;
+//
+//echo "Сложение:" . ($a + $b);
+//echo "Вычитание:" . ($a - $b);
+
+//$array = ['a'=>'Sergey','b'=>'Alexander','c'=>'Alexey','z'=>'Anna', 't'=>'Tanya'];
+//ksort($array);
+//krsort($array);
+//asort($array);
+//arsort($array);
+//$chunks = array_chunk($array,2, true);
+//$diff = array_diff($array1,$array2);
+
+//$array1 = ['a','b','c','d','e','f','g','a','a','f',];
+//$array2 = ['h','i','j','k','a','b','c','f'];
+//$merge = array_unique(array_merge($array1,$array2));
+//echo "<pre>";
+//print_r($merge);
+//echo "</pre>";
+
+//$fruits = ['apple','bananna','orange','pear'];
+//$pop = array_pop($fruits);
+//echo "<pre>";
+//echo $pop;
+//echo "</pre>";
+//echo "<pre>";
+//var_dump ($fruits);
+//echo "</pre>";
+
+//$fruits = ['apple','bananna','orange','pear'];
+//$shift = array_shift($fruits);
+//echo "<pre>";
+//print_r ($fruits);
+//echo "</pre>";
+
+//$fruits = ['apple','bananna','orange','pear'];
+//shuffle($fruits);
+//print_r($fruits);
+
+//$fruits = ['apple','bananna','orange','pear'];
+//$rand = array_rand($fruits);
+//print_r($fruits[$rand]);
+
+//$number = 9;
+//
+//if ($number > 10){
+//    print "bigger then 10";
+//}elseif($number >5){
+//    print "betwin 5 and 10";
+//}else{echo"lower then 5";}
+
+//echo "<pre>";
+//echo $number > 10 ? "yes" : "no";
+//echo "</pre>";
+
+$name = "Sergey";
+    switch ($name){
+        case "Sergey":
+            echo "Hello,".$name;
+            break;
+    }
 
 ?>
