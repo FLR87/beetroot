9b1627e5a78d9c452d852dcb2ddbfbb703f6f00e
FLR87 <galeonis87@gmail.com>
2017-11-22 10:25:39 +0200

    callback function example with commentaries

--- index.php
+++ index.php
@@ -1,40 +1,4 @@
 <?php
 
-//$int = 1;
-//$string = '23';
-
-$array = [1, 3, 5, 6, 7, 8, 9, 'users' => ['Vadim', 'Alex', 'Sanya']];
-
-//$arrayNames = ['Petrov','Ivanov','Sidorov'];
-//for ($i = 0; $i < 15; $i++) {
-//    echo "<pre>";
-//    var_dump($array[$i]);
-//    echo "</pre>";
-
-//}
-
-foreach ($array as $key => $value) {
-    if (is_array($value)) {
-        foreach ($value as $key => $value) {
-            var_dump($value);
-        }
-    }
-
-}
-//while ($i < 5) {
-//    $count = $i++;
-//    echo '<pre>';
-//    var_dump($i);
-//    echo '</pre>';
-//}
-//$array = [1, 3, 5, 6, 7, 8, 9, 'fags'=> 10, 'users' => ['Vadim', 'Alex', 'Sanya']];
-//foreach ($array as $key => $value) {
-//    echo '<pre>';
-//    var_dump($value);
-//    echo '</pre>';
-//
-//}
-
-
 
 ?>
