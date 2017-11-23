c2f8a37600b9397cc262013840e74ae77092fe49
FLR87 <galeonis87@gmail.com>
2017-11-23 10:54:36 +0200

    1_1_done.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
+//Реализуйте функцию подсчета количества элементов в массиве
+$array = [0, '1', 2, '3', 4, '5', 6, '7', 8, '9',50,'text'];
+
+    function counTer($array)
+    {
+        $n = 0;
+        foreach ($array as $val) {
+            $n++;
+        }
+        return $n;
+    }
+    echo counTer($array);
 
 ?>
