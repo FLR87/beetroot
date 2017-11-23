cfd0bb89dbc04861659949155b1bc8e194b4fa2c
FLR87 <galeonis87@gmail.com>
2017-11-23 11:30:23 +0200

    1_2_done.php

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 
+//Реализуйте функцию вывода положительных значений массива кратных «5»
+//Basic array:
+$array=[0,-10,-13, 20,-40, 30, 40, 50, 60, 70, 80, 100];
+//Multyplicity:
+$mlt=5;
+
+//calling mlTfunction:
+mlTfunction($array,$mlt);
+
+//Setting up mlTfunction:
+function mlTfunction ($array, $mlt){
+    foreach ($array as $val){
+        if(($val >= 0) && ($val % $mlt == 0)){
+            echo "<pre>";
+            echo "$val";
+            echo "</pre>";
+        }
+    }
+}
 
 ?>
