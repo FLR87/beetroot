6863799d31f31ffe7a3eef9ddbc9e13f2174a4ca
FLR87 <galeonis87@gmail.com>
2017-11-23 12:05:51 +0200

    true_false_nmbrDispenser_to_files.php

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 
+//Инициализируйте массив с целочисленными значениями.
+//Реализуйте функцию записи положительных и отрицательных значений в разные файлы использую оператор *switch*
+//Basic array;
+$array = [1,-2,5,-10,20,-50,100,-125,150,-100500];
+//Function initialization:
+numbrDispenser($array);
+
+//Function setup:
+function numbrDispenser($array){
+    file_put_contents('negative.txt','');
+    file_put_contents('positive.txt','');
+
+    foreach ($array as $val){
+        switch ($val > 0){
+            case true:
+                file_put_contents('positive.txt', $val . '>0 </br>', FILE_APPEND | LOCK_EX);
+                echo $val . '>0 </br>';
+                break;
+            case false:
+                file_put_contents('negative.txt',$val . '<0 </br>', FILE_APPEND| LOCK_EX);
+                echo $val . '<0 </br>';
+                break;
+        }
+    }
+    return true;
+}
 
 ?>
