7f7d8ad3f648c6d1efeb6685b605e182e50115bd
FLR87 <galeonis87@gmail.com>
2017-11-23 12:17:10 +0200

    bubble sort thing in a nutshell.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
+/**
+ * Created by PhpStorm and officialy stolen from Wiki
+ * User: coolhaker
+ * Date: 21.11.2017
+ * Time: 16:18
+ */
+//Bubble Sort in a nutshell:
+$array = [0, 9, 1, 20, 15, 3, 4, 5];
+for ($i = 0; $i < count($array); $i++) {
+    for ($j = $i + 1; $j < count($array); $j++) {
+        if ($array[$i] > $array[$j]) {
+            $temp = $array[$j];
+            $array[$j] = $array[$i];
+            $array[$i] = $temp;
+        }
+    }
+}
+print_r($array);
 
 ?>
