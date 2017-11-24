8e722d1d3a5cb92ddc4d426608056cf5795a4567
FLR87 <galeonis87@gmail.com>
2017-11-24 18:17:36 +0200

    first_callback_positive/negative_stringfilter

--- index.php
+++ index.php
@@ -1,22 +1,47 @@
 <?php
+$array = [1, -1, 'key', 25, -25, 'waffle', 50, -50, '(^_^)'];
 
-/**
- * Created by PhpStorm and officialy stolen from Wiki
- * User: coolhaker
- * Date: 21.11.2017
- * Time: 16:18
- */
-//Bubble Sort in a nutshell:
-$array = [0, 9, 1, 20, 15, 3, 4, 5];
-for ($i = 0; $i < count($array); $i++) {
-    for ($j = $i + 1; $j < count($array); $j++) {
-        if ($array[$i] > $array[$j]) {
-            $temp = $array[$j];
-            $array[$j] = $array[$i];
-            $array[$i] = $temp;
-        }
-    }
-}
+print_r('Unprocessed array </br>');
 print_r($array);
 
+$positiveFunction = function ($var) {
+    foreach ($var as $value) {
+        // is value positive;
+        if ($value >= 0) {
+            $result[] = $value;
+        }
+    }
+    return $result;
+};
+$negativeFunction = function ($var) {
+    foreach ($var as $value ){
+        // is value negative;
+        if ($value < 0) {
+            $result[] = $value;
+        }
+    }
+    return $result;
+};
+function stringFilter($array1, $callback = null)
+{
+    // String values has been filtered away:
+    for ($i = 0; $i < count($array1); $i++) {
+        If (is_numeric($array1 [$i])) {
+            $result[] = $array1 [$i];
+        }
+    }
+    //If callable execute functions:
+    if (is_callable($callback)) {
+        //Calling user's function:
+        $result = call_user_func($callback, $result);
+    }
+    return $result;
+}
+
+echo('<br><br> Массив состоящий только из чисел (фильтр исходного)<br>');
+print_r(stringFilter($array));
+echo('<br><br> Числа массива, которые БОЛЬШЕ нуля <br>');
+print_r(stringFilter($array, $positiveFunction));
+echo('<br><br> Числа массива, которые МЕНЬШЕ нуля <br>');
+print_r(stringFilter($array, $negativeFunction));
 ?>
