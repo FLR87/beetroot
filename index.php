7e5e4b2cb9d303d1f5158b31ac52857cef89c2d1
FLR87 <galeonis87@gmail.com>
2017-12-13 12:33:11 +0200

    OOP classwork

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <?php
 
+class Car {
+    public $brand = 'BMW';
+    public $speed = '100';
+    public $color = 'red';
+    public $fuel = 15;
+
+    function __construct($brand,$speed,$color,$fuel){
+        $this->brand = $brand;
+        $this->speed = $speed;
+        $this->color = $color;
+        $this->fuel = $fuel;
+    }
+
+    public function getBrand(){
+        return $this->brand;
+    }
+    public function setBrand($brand){
+        return $this->brand = $brand;
+    }
+
+}
+
+$carOneObject = new Car('AUDI','300','black',20);
+echo $carOneObject->Brand;
+
+//$carTwoObject = new Car('MERS','200','blue',10);
+
+
+echo '<pre>';
+var_dump($carOneObject);
+echo '</pre>';
+
 
 ?>
